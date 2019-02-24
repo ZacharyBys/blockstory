843ef91032f76b9b8b9b506c7d1a393cca2fb379
zacharybys <zachary.bys@gmail.com>
2019-02-24 04:29:48 -0500

    Added a few more icons

--- blockstory/src/components/Contributor.js
+++ blockstory/src/components/Contributor.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { List, Image } from 'semantic-ui-react';
 
 const Contributor = (props) => {
-    const icons = ['https://react.semantic-ui.com/images/avatar/small/matthew.png', 'https://react.semantic-ui.com/images/avatar/small/lindsay.png', 'https://react.semantic-ui.com/images/avatar/small/christian.jpg']
+    const icons = ['https://react.semantic-ui.com/images/avatar/small/matthew.png', 'https://react.semantic-ui.com/images/avatar/small/lindsay.png', 'https://react.semantic-ui.com/images/avatar/small/christian.jpg', 'https://react.semantic-ui.com/images/avatar/small/matt.jpg', 'https://react.semantic-ui.com/images/avatar/small/helen.jpg', 'https://react.semantic-ui.com/images/avatar/small/daniel.jpg']
     return (
         <List.Item>
             <Image avatar src={icons[props.num]} />
